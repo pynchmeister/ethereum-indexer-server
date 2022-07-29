8830fde65f30da0088de5c81bce1b4f58931035d
wighawag <wighawag@gmail.com>
2022-07-29 21:40:41 +0100

    setup

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import * as Koa from "koa";
+import * as Router from "koa-router";
+
+import * as logger from "koa-logger";
+import * as json from "koa-json";
+import * as bodyParser from "koa-bodyparser";
+
+const app = new Koa();
+const router = new Router();
+
+interface HelloRequest {
+  name: string;
+}
+
+// Hello world
+router.post("/", async (ctx, next) => {
+  const { name } = <HelloRequest>ctx.request.body;
+  ctx.body = { name, test: 1 };
+  await next();
+});
+
+router.get("/", async (ctx, next) => {
+    const { name } = {name: 'hhh'};
+    ctx.body = { name, test: 1 };
+    await next();
+});
+
+// Middlewares
+app.use(json());
+app.use(logger());
+app.use(bodyParser());
+
+// Routes
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log("Koa started");
+});
